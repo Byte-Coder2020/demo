3bd6e68fd8782fbbdcd1f6fb8841174e23f5fa68
Byte-Coder2020 <61932418+Byte-Coder2020@users.noreply.github.com>
2021-10-04 20:55:30 +0800

    123

--- pyplot.py
+++ pyplot.py
@@ -9,8 +9,4 @@ for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-frequencies = []
-for value in range(1, die.num_sides+1):
-    frequency = results.count(value)
-    frequencies.append(frequency)
 
